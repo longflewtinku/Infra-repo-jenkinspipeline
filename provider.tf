5aa187b1dbcb142dcf599358321bb3719a4697c7
laxmikanth <laxitinku7788@gmail.com>
2025-10-15 12:24:13 +0530

    updated infra pipeline-2

--- provider.tf
+++ provider.tf
@@ -5,12 +5,12 @@ terraform {
       version = "6.16.0"
     }
   }
-  backend "s3" {
-    bucket = "1996-july-28-bucket"
-    key = JP/Infraterraform
-    region = "ap-south-1"
-    encrypt = true
-  }
+#   backend "s3" {
+#     bucket = "1996-july-28-bucket"
+#     key = JP/Infraterraform
+#     region = "ap-south-1"
+#     encrypt = true
+#   }
 }
 provider "aws" {
     region  = "ap-south-1"
